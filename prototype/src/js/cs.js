f1edb3a5856250e289b4ea3f41fc96d799124d0c
Olivia Artz <liv@oliviaartz.com>
2020-01-16 15:32:18 -0500

    timeline logic coming together…

--- prototype/src/js/cs.js
+++ prototype/src/js/cs.js
@@ -1,35 +1,51 @@
 // $( function() {
 
-    var topics = [],
-        projects = [],
-        $topicLinks = $( '[data-topic-link' ),
-        $projects = $( '#projects article' ),
-        DateTime = luxon.DateTime,
-        timelineStart = Infinity,
-        timelineEnd = -Infinity
+	var topics = [],
+		projects = [],
+		$topicLinks = $( '[data-topic-link' ),
+		$projects = $( '#projects article' ),
+		DateTime = luxon.DateTime,
+		timelineStart = Infinity,
+		timelineEnd = -Infinity
 
-    $topicLinks.each( function() {
-      topics.push( $( this ).attr( 'data-topic-link' ) )
-    })
+	$topicLinks.each( function() {
+		topics.push( $( this ).attr( 'data-topic-link' ) )
+	})
 
-    $projects.each( function() {
-        var $this = $( this ),
-            thisStart = DateTime.fromISO( $this.data( 'project-start-date' ) ),
-            thisEnd = DateTime.fromISO( $this.data( 'project-end-date' ) )
+	$projects.each( function() {
+		var $this = $( this ),
+			thisStart = DateTime.fromISO( $this.data( 'project-start-date' ) ),
+			thisEnd = DateTime.fromISO( $this.data( 'project-end-date' ) )
 
-            timelineStart = Math.min( timelineStart, thisStart)
-            timelineEnd = Math.max( timelineEnd, thisStart)
+			timelineStart = Math.min( timelineStart, thisStart)
+			timelineEnd = Math.max( timelineEnd, thisEnd)
 
-        projects.push( {
-            'start': thisStart,
-            'end': thisEnd,
-            'topics': $( this ).data( 'project-topics' ).split( ',' )
-        } )
-    })
+		projects.push( {
+			'start': thisStart,
+			'end': thisEnd,
+			'topics': $( this ).data( 'project-topics' ).split( ',' ),
+			'$el': $this
+		} )
+	})
 
-    // Now create the timelineRanges based on the relative position of the 
-    //  projects[n].start/end properties relative to the timelineStart/End
+	// Now create the timelineRanges based on the relative position of the 
+	//  projects[n].start/end properties relative to the timelineStart/End
+	for( var i = 0; i < projects.length; i++ ) {
+		var $timelineRange = $( '<div class="timelineRange" />' )
+		$timelineRange.css( {
+			'top': timeToPercent( projects[i].start.ts ) + '%',
+			'bottom': timeToPercent( projects[i].end.ts ) + '%'
+		} )
+		$timelineRange.appendTo( '#timelines' )
+	}
 
-    console.log( topics, projects, timelineStart, timelineEnd )
+	function timeToPercent( value, r1, r2 ) { 
+		console.log(value)
+		var r1 = r1 || [timelineStart, timelineEnd],
+		r2 = r2 || [0, 100]
+		return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ]
+	}
+
+	console.log( topics, projects, timelineStart, timelineEnd )
 
 // } )
