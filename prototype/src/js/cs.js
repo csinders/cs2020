75cebc80bba91927ca56b5de35748560efdc16e7
Olivia Artz <liv@oliviaartz.com>
2020-01-21 20:46:38 -0500

    im gay

--- prototype/src/js/cs.js
+++ prototype/src/js/cs.js
@@ -4,46 +4,59 @@
 		projects = [],
 		$topicLinks = $( '[data-topic-link' ),
 		$projects = $( '#projects article' ),
+		$timelines = $( '#timelines' ),
 		DateTime = luxon.DateTime,
 		timelineStart = Infinity,
 		timelineEnd = -Infinity
 
-	$topicLinks.each( function() {
-		topics.push( $( this ).attr( 'data-topic-link' ) )
-	})
-
-	$projects.each( function() {
-		var $this = $( this ),
-			thisStart = DateTime.fromISO( $this.data( 'project-start-date' ) ),
-			thisEnd = DateTime.fromISO( $this.data( 'project-end-date' ) )
-
-			timelineStart = Math.min( timelineStart, thisStart)
-			timelineEnd = Math.max( timelineEnd, thisEnd)
-
-		projects.push( {
-			'start': thisStart,
-			'end': thisEnd,
-			'topics': $( this ).data( 'project-topics' ).split( ',' ),
-			'$el': $this
-		} )
-	})
-
-	// Now create the timelineRanges based on the relative position of the 
-	//  projects[n].start/end properties relative to the timelineStart/End
-	for( var i = 0; i < projects.length; i++ ) {
-		var $timelineRange = $( '<div class="timelineRange" />' )
-		$timelineRange.css( {
-			'top': 100 - timeToPercent( projects[i].end.ts ) + '%',
-			'bottom': timeToPercent( projects[i].start.ts ) + '%'
-		} )
-		$timelineRange.appendTo( '#timelines' )
+	var prepTheTopics = function() {
+		$topicLinks.each( function() {
+			topics.push( $( this ).attr( 'data-topic-link' ) )
+			$timelines.append( $( '<div class="timelineTopic" />'))
+		})
 	}
 
-	function timeToPercent( value, r1, r2 ) { 
-		var r1 = r1 || [timelineStart, timelineEnd],
-		r2 = r2 || [0, 100]
-		return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ]
+	var prepTheProjects = function() {
+		$projects.each( function() {
+			var $this = $( this ),
+				thisStart = DateTime.fromISO( $this.data( 'project-start-date' ) ),
+				thisEnd = DateTime.fromISO( $this.data( 'project-end-date' ) )
+	
+				timelineStart = Math.min( timelineStart, thisStart)
+				timelineEnd = Math.max( timelineEnd, thisEnd)
+	
+			projects.push( {
+				'start': thisStart,
+				'end': thisEnd,
+				'topics': $( this ).data( 'project-topics' ).split( ',' ),
+				'$el': $this
+			} )
+		})	
 	}
+
+	var prepTheTimeRanges = function() {
+		// Now create the timelineRanges based on the relative position of the 
+		//  projects[n].start/end properties relative to the timelineStart/End
+		for( var i = 0; i < projects.length; i++ ) {
+			var $timelineRange = $( '<div class="timelineRange" />' )
+			$timelineRange.css( {
+				'top': 100 - timeToPercent( projects[i].end.ts ) + '%',
+				'bottom': timeToPercent( projects[i].start.ts ) + '%'
+			} )
+			$timelineRange.appendTo( '#timelines' )
+		}
+
+		function timeToPercent( value, r1, r2 ) { 
+			var r1 = r1 || [timelineStart, timelineEnd],
+			r2 = r2 || [0, 100]
+			return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ]
+		}
+	}
+
+
+	prepTheTopics()
+	prepTheProjects()
+	prepTheTimeRanges()
 
 	console.log( topics, projects, timelineStart, timelineEnd )
 
