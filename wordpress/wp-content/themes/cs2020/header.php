f03b9248afd533ad3bc5b41b1de0dcb2a77cf70d
Olivia Artz <liv@oliviaartz.com>
2020-05-13 20:50:05 -0400

    Tweaks to favicon

--- wordpress/wp-content/themes/cs2020/header.php
+++ wordpress/wp-content/themes/cs2020/header.php
@@ -8,7 +8,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-16x16.png">
-    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/favicon/safari-pinned-tab.svg" color="#ffffff">
     <link rel="shortcut icon" href="/<?php echo get_template_directory_uri(); ?>/favicon/favicon.ico">
     <meta name="og:image" content="<?php echo get_template_directory_uri(); ?>/favicon/og-image.png">
     <?php wp_head();?>
